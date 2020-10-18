c42a9f6ae32e385f7af7d4933793b0d3940ec872
נורית גרינברג <nurit@g.jct.ac.il>
2020-10-19 02:36:44 +0300

    חומרים לשיעור שני

---
+++ solid13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid13
+{
+
+    class Animal { }
+
+    class Dog : Animal { }
+
+    class Program
+    {
+        static void PrintTypes(Animal a)
+        {
+            Console.WriteLine(a.GetType() == typeof(Animal)); // false 
+            Console.WriteLine(a is Animal);                  // true 
+            Console.WriteLine(a.GetType() == typeof(Dog));   // true
+        }
+
+        static void Main(string[] args)
+        {
+            Dog spot = new Dog();
+            PrintTypes(spot);
+        }
+    }
+}
---
+++ solid20/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace solid20
+{
+    /// <summary>
+    /// דוגמא של מאפיין property 
+    /// מקביל לדוגמא EX95
+    /// </summary>
+    class MyClass
+    {
+        private int m_x;
+        public int X
+        {
+            get { return m_x; }
+            set { m_x = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            myClass.X = 8;
+            Console.WriteLine(myClass.X);
+        }
+    }
+}
